ab4045907a5eb96e4df7fa584a76c8b9ae573be5
Andrew Menning <menning23@gmail.com>
2017-03-02 23:25:34 -0500

    remove model test from minitest

--- test/models/dice_test.rb
+++ test/models/dice_test.rb
@@ -1,12 +1,7 @@
 require 'test_helper'
 
 class DiceTest < ActiveSupport::TestCase
-  test 'should create dice with random value with range of 1 to 6' do
-    @dice = Dice.new
-    @dice.createRandomDice
-
-    assert_not_nil @dice.value
-    expect(@dice.value).to be <= 6
-    expect(@dice.value).to be >= 1
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
