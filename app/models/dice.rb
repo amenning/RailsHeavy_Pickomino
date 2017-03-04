19513be6891f3ede1f15bff1c66a310370b29acc
Andrew Menning <menning23@gmail.com>
2017-03-04 01:04:40 -0500

    add new game_parameters yml file

--- app/models/dice.rb
+++ app/models/dice.rb
@@ -3,7 +3,8 @@ class Dice < ActiveRecord::Base
   belongs_to :frozen_dice
 
   def create_random_dice
-    self.value = rand(1..6)
+    game_parameters = Rails.configuration.x.game_parameters
+    self.value = rand(game_parameters['min_dice_value']..game_parameters['max_dice_value'])
     save
   end
 end
