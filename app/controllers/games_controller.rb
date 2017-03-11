318814b4c0f942b35ac1410b8375ab8a840e2d8a
Andrew Menning <menning23@gmail.com>
2017-03-11 12:36:08 -0500

    convert active dice into clickable buttons

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,8 +11,69 @@ class GamesController < ApplicationController
 
   def roll
     @active_dice = GamesHelper.get_active_dice_hash
+    # Check if bunk
+    # Disable roll button
+    # @player_options = { roll: false, clearnBunk: true?, message: message }
     respond_to do |format|
       format.js { render 'roll' }
+    end
+  end
+
+  def freeze_dice
+    @active_dice = GamesHelper.get_active_dice_hash_after_freeze
+    @frozen_dice = GamesHelper.get_frozen_dice_hash_after_freeze
+    # Verify dice number grouping not already frozen
+    # Move dice from active set to frozen set
+    # Enable worm take action
+    # Enable roll button
+    # @active_dice
+    # @frozen_dice
+    # @player_options = { roll: true, message: message }
+    respond_to do |format|
+      format.js { render 'freeze_dice' }
+    end
+  end
+
+  def take_worm
+    # Verify frozen dice has worm
+    # Verify forzen dice total is equal to or greater
+    # Move worm from grill set to player worm set
+    # Move frozen dice to active dice
+    # Check for game end
+    # Enable roll button
+    # @grill_worms
+    # @player_worms
+    # @active_dice
+    # @frozen_dice
+    # @player_options = { roll: true, message: message }
+    respond_to do |format|
+      format.js { render 'take_worm' }
+    end
+  end
+
+  def bunk
+    # Check if player has any worm tiles
+    # If player has worm, make largest grill worm tile dead
+    # Move player worm to grill set
+    # Enable roll button
+    # @grill_worms
+    # @player_worms
+    # @active_dice
+    # @frozen_dice
+    # @player_options = { roll: true, message: message }
+    respond_to do |format|
+      format.js { render 'bunk' }
+    end
+  end
+
+  def game_over
+    # @grill_worms
+    # @player_worms
+    # @active_dice
+    # @frozen_dice
+    # @player_options = { reset: true, message: message }
+    respond_to do |format|
+      format.js { render 'game_over' }
     end
   end
 end
