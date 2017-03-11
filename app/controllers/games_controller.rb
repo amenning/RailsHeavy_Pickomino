9bcdf2f8206abdba52fbb80841f9a9bd67d40d2e
Andrew Menning <menning23@gmail.com>
2017-03-11 01:43:09 -0500

    initialize empty frozen dice and player worms

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < ApplicationController
   def play
     @active_dice = GamesHelper.get_active_dice_hash(true)
+    @frozen_dice = {}
     @grill_worms = GamesHelper.get_grill_worms_hash(true)
+    @player_worms = {}
     respond_to do |format|
       format.html { render :game_board }
     end
