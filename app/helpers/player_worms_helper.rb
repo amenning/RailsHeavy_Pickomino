08f0363f417aad828851644b7c57ba2979a38a0d
Andrew Menning <menning23@gmail.com>
2017-03-14 20:05:17 -0400

    move take worm methods to TakeWorm interactor

--- app/helpers/player_worms_helper.rb
+++ app/helpers/player_worms_helper.rb
@@ -1,34 +1,11 @@
 module PlayerWormsHelper
   def self.get_player_worms_hash_after_claim(worm_value)
-    @worm_value = worm_value
-    @grill = Grill.last
     @player_worm_set = PlayerWormSet.last
-    move_worm_from_grill_to_player
+    TakeWorm.call(worm_value: worm_value, player_worm_set: @player_worm_set)
     create_player_worm_hash
   end
 
   private
-
-  def self.move_worm_from_grill_to_player
-    @grill.grill_worm.all.each do |grill_worm|
-      worm = grill_worm.worm.last
-      next unless worm.value == @worm_value
-      create_new_player_worm_and_associate_with_worm(worm)
-      unassociated_grill_worm_and_destroy(grill_worm, worm)
-    end
-  end
-
-  def self.create_new_player_worm_and_associate_with_worm(worm)
-    player_worm = @player_worm_set.player_worm.create
-    worm.player_worm_id = player_worm.id
-    worm.save
-  end
-
-  def self.unassociated_grill_worm_and_destroy(grill_worm, worm)
-    worm.grill_worm_id = nil
-    worm.save
-    grill_worm.destroy
-  end
 
   def self.create_player_worm_hash
     player_worm_hash = @player_worm_set.player_worm.all.to_a.reverse_each.map do |player_worm|
---
+++ app/interactors/take_worm.rb
@@ -0,0 +1,33 @@
+class TakeWorm
+  include Interactor
+
+  def call
+    @worm_value = context.worm_value
+    @player_worm_set = context.player_worm_set
+    @grill = Grill.last
+    move_worm_from_grill_to_player
+  end
+
+  private
+
+  def move_worm_from_grill_to_player
+    @grill.grill_worm.all.each do |grill_worm|
+      worm = grill_worm.worm.last
+      next unless worm.value == @worm_value
+      create_new_player_worm_and_associate_with_worm(worm)
+      unassociated_grill_worm_and_destroy(grill_worm, worm)
+    end
+  end
+
+  def create_new_player_worm_and_associate_with_worm(worm)
+    player_worm = @player_worm_set.player_worm.create
+    worm.player_worm_id = player_worm.id
+    worm.save
+  end
+
+  def unassociated_grill_worm_and_destroy(grill_worm, worm)
+    worm.grill_worm_id = nil
+    worm.save
+    grill_worm.destroy
+  end
+end
