daf2accca4a60d4980c602a8f52d1ea5e6219b7f
Andrew Menning <menning23@gmail.com>
2017-03-16 23:01:55 -0400

    Reduce db calls using ||= operator on theme

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,15 +1,15 @@
 module ImagesHelper
   def self.get_dice_image(dice_value)
-    theme = ThemesHelper.get_theme
-    dice_image = theme.image.where(
+    @theme ||= ThemesHelper.get_theme
+    dice_image = @theme.image.where(
       'target LIKE ?', '%' + dice_value.to_s + ' pip die face%'
     ).take
     get_filename_with_filetype(dice_image)
   end
 
   def self.get_worm_tile_image(worm_count)
-    theme = ThemesHelper.get_theme
-    worm_image = theme.image.where(
+    @theme ||= ThemesHelper.get_theme
+    worm_image = @theme.image.where(
       'target LIKE ?', '%' + worm_count.to_s + ' worm tile%'
     ).take
     get_filename_with_filetype(worm_image)
