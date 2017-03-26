e98533bfe1739a40b29f538d2461529f82fff464
Andrew Menning <menning23@gmail.com>
2017-03-25 23:00:18 -0400

    update frozen_dices helper class

--- lib/helpers/frozen_dices.rb
+++ lib/helpers/frozen_dices.rb
@@ -6,6 +6,11 @@ module Helpers
       create_frozen_dice_hash
     end
 
+    def frozen_dice_sum(frozen_dice_values)
+      result = SumFrozenDice.call(frozen_dice_values: frozen_dice_values)
+      result.frozen_dice_sum
+    end
+
     private
 
     def create_frozen_dice_hash
