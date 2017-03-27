4a9dcae988148ec61bd424f60aae0629c6096550
Andrew Menning <menning23@gmail.com>
2017-03-26 21:08:53 -0400

    add spec for freeze_dice interactor

--- spec/interactors/freeze_dice_spec.rb
+++ spec/interactors/freeze_dice_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe FreezeDice, type: :Interactor do
   fixtures :frozen_dice_sets, :frozen_dices, :active_dice_sets, :active_dices, :dices
 
   context 'when given valid frozen_dice_set, and dice_value in last active_dice_set' do
-    it 'should successfully move dice from actie to frozen' do
+    it 'should return success' do
       context = FreezeDice.call(
         dice_value: 3,
         frozen_dice_set: FrozenDiceSet.find(3)
