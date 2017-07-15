c815593aaf2bbc069f8b764c419e61383cf3223f
Andrew Menning <menning23@gmail.com>
2017-07-14 23:00:06 -0400

    Update precompiled assets

--- public/assets/application-172b7c1cf163f791a7f22960dc87f2cef12a00ac5a8b514c18d2e98674da9eba.js
+++ public/assets/application-172b7c1cf163f791a7f22960dc87f2cef12a00ac5a8b514c18d2e98674da9eba.js
@@ -12792,6 +12792,9 @@ if (typeof jQuery === 'undefined') {
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 ;
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+;
 
 
 
